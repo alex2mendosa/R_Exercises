c71826214a84760fb6ac8ba757a1a48689feed89
alex2mendosa <66001736+alex2mendosa@users.noreply.github.com>
2020-09-01 16:29:35 +0300

    Update Data_frame_index.R

--- Data_frame_index.R
+++ Data_frame_index.R
@@ -7,20 +7,22 @@ vec_1<-sample(20:30,10,replace=TRUE)
 vec_2<-sample(letters,10,replace=TRUE)
 sample_frame<-data.frame(digits=vec_1,letters=vec_2)
 
-Sample function randomly select 10 elements from 
+sample() function randomly select 10 elements from 
 vectors 20:30 and letters.lettets is vector
 already build in R.
 
-
-Write a R expressiom to extract 
-column 2 from a data frame using
+Write 2 R expressions to extract 
+column 2 and all rows from a data frame using
 column name and column number.
-Use $ immediately aftet data frame name followed
-by column name you want to extract
+Use $ immediately after data frame name followed
+by column name you want to extract.
 
 sample_frame$letters
 
-
+Notice that in case we extract 1 column, 
+output would represent atomic vector which
+has the same type as the column which was
+extracted.
 
 
 # Exercise 2
@@ -29,27 +31,29 @@ Run the following code to generate
 3 vectors of random numbers and letters.
 
 vec_1<-sample(20:30,10,replace=TRUE)
-vec_2<-sample(letters,10,replace=TRUE)
-vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE) # vector of character class
+vec_3<-sample(LETTERS,10,replace=TRUE) # vector of character class
 sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3)
 
-
-Write a R expressiom to extract 
-second column , next write expression to 
+Write  R expression to extract 
+second column by column number, next write expression to 
 extract only columns 1 and 3 from a data frame using
-columns column numbers.
+vectors with columns numbers.
 Try to use single expression.
 
-sample_frame[,2]
+sample_frame[,2]  
+# Notice that class of vec_2 and vec_3 are characters
+# however when combine in data frame character class is 
+# retransformed to factor class.
+# Extracted columns now also inherit factor class.
+                 
 sample_frame[,c(1,3)]
 
 Data frame has 2 dimentions, numbers of rows and columns,
 therefore, in square brackets you first indicate vector with wor numbers, 
-next you indoicate vector with column  numbers. If row of column index is not mentioned,
+next you indoicate vector with column  numbers. 
+If row of column index is not mentioned,
 all rows or all columns are dispolayed. 
-
-
-
 
 
 # Exercise 3
@@ -63,13 +67,229 @@ vec_3<-sample(LETTERS,10,replace=TRUE)
 sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3)
 
 
-Write a R expressiom to extract 
-forst  and second columns usinf $ and column name
-ro referenced required columns.
+Write R expressiom to extract 
+first and second columns using names
+referenced by character vector.
 Try to use single expression.
 
-
 sample_frame[,c("digits","case_high")]
+
+
+
+# Exercise 4
+
+Use  the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE) # vector of character class
+vec_3<-sample(LETTERS,10,replace=TRUE) # vector of character class
+
+Write  R expressions which do the following:
+
+1.Create data frame named sample_frame where columns are named
+digits, case_low,case_high.
+use str() fucntion to check type of data for each column.
+
+2. Repeat task 1, but add to data frame
+fucntion argument stringsAsFactors=FASLE.
+use str() again to check if column naw have different class.
+
+3. Extract columns 1 and 3 from a data frame using
+vectors with columns numbers.
+
+sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3)  
+str(sample_frame)
+
+sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3,
+                        stringsAsFactors=FASLE)  
+str(sample_frame)
+
+sample_frame[,c(1,3)]
+
+
+
+
+# Exercise 4
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3)
+
+
+Write 2 R expressiom to extract 
+second and fifth row with, 1-st and 4-th row where
+infices are referenced in numeric vector. 
+
+sample_frame[c(2,5),]
+sample_frame[c(1,4),]
+
+Notice, we indicate rows, commo,
+clunns are not referenced, therefore 
+we extract all columns. 
+
+
+
+# Exercise 5
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+sample_frame<-data.frame(digits=vec_1,case_low=vec_2,case_high=vec_3)
+
+when length() fucnton is applied to data frame , output
+indicated total number of columns. 
+
+Write 2  R expressiom to extract 
+first and last columns, second and penultimate column. Use length fucntion 
+to estimate indices of columns.
+
+sample_frame[,c(1,length(sample_frame) )]
+sample_frame[,c(2,length(sample_frame)-1 )]
+
+Because we did not indocate specific rows, 
+R reads as as instruction to eactract all rows. 
+
+
+
+# Exercise 6
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_4<-sample( c(TRUE,FALSE) ,10,replace=TRUE)
+
+sample_frame_1<-data.frame(digits=vec_1,case_low=vec_2, stringsAsFactors=FALSE)
+sample_frame_2<-data.frame(case_high=vec_3,y_logic=vec_4, stringsAsFactors=FALSE)
+
+
+Use $ to access columns of each data frame,
+and use them as argument of data frame fucntion to binf 
+vectors in the following order
+
+digits,y_logic,case_low,case_high
+
+forming a data frame called sample_frame_3
+
+Next use name fcuntion to rename columns a new composed data
+
+sample_frame_3<-data.frame(sample_frame_1$digits,sample_frame_2$y_logic,
+            sample_frame_1$case_low,sample_frame_2$case_high)
+
+names(sample_frame_3)<-c("digits","y_logic","case_low","case_high")
+
+
+
+
+
+# Exercise 7
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_4<-sample( c(TRUE,FALSE) ,10,replace=TRUE)
+
+sample_frame_1<-data.frame(vec_1,vec_2,
+            vec_3,vec_4,stringsAsFactors=FALSE)
+names(sample_frame_1)<-c("digits","case_low","case_high","y_logic")
+
+
+Use vector with indices to extract
+element in 4-th row 2-nd column
+
+sample_frame_1[4,2] or sample_frame_1[4,"case_low"]
+sample_frame_1[5,4] or sample_frame_1[4,"case_low"]
+
+
+# Exercise 8
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_4<-sample( c(TRUE,FALSE) ,10,replace=TRUE)
+
+sample_frame_1<-data.frame(vec_1,vec_2,
+            vec_3,vec_4)
+names(sample_frame_1)<-c("digits","case_low","case_high","y_logic")
+
+
+Use vector with indices to extract
+1-st and 4-th row and 1-st and 3-rd columns
+
+sample_frame_1[c(1,4),c(1,3)]
+
+
+
+# Exercise 9
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_4<-sample( c(TRUE,FALSE) ,10,replace=TRUE)
+
+sample_frame_1<-data.frame(vec_1,vec_2,
+            vec_3,vec_4)
+names(sample_frame_1)<-c("digits","case_low","case_high","y_logic")
+
+
+use seq() fucntion to create vector of uneneb numbers abd use it to extract 
+uneven rows from colimn 1 and 3
+
+loc.1<-seq(from=1,to=10,by=2)
+sample_frame_1[loc.1,c(1,3)]
+
+
+
+
+
+
+# Exercise 10
+
+Run the following code to generate
+3 vectors of random numbers and letters.
+
+vec_1<-sample(20:30,10,replace=TRUE)
+vec_2<-sample(letters,10,replace=TRUE)
+vec_3<-sample(LETTERS,10,replace=TRUE)
+vec_4<-sample( c(TRUE,FALSE) ,10,replace=TRUE)
+
+sample_frame_1<-data.frame(vec_1,vec_2,
+            vec_3,vec_4)
+names(sample_frame_1)<-c("digits","case_low","case_high","y_logic")
+
+
+use seq() fucntion to create vector of even  numbers abd use it to extract 
+even rows from all columns, coluns shopuld be in reverse order
+
+loc.1<-seq(from=2,to=10,by=2)
+sample_frame_1[loc.1,4:1]
+
+
+
+
+
+
+
+
 
 
 
