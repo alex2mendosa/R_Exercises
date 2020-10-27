a959ac5ce4a4e0e42750cd90c71f770a526e2450
alex2mendosa <66001736+alex2mendosa@users.noreply.github.com>
2020-10-27 14:33:31 +0200

    Update Loops_R.R

--- Loops_R.R
+++ Loops_R.R
@@ -1,3 +1,4 @@
+#1
 Most common way to create loop is to use for keyword.
 for loop syntax in R is represented in the following
 form
@@ -33,16 +34,135 @@ would allow more customised data manipulations.
 
 
 
-#2
-vec<-se
-for ( i in  )
+# 2
+For loop would allow to create sequences for
+which each consequtive value is the result
+of arithmetic or logical operation performed over previous values
+
+ct_1=0
+for (i in 1:10 ){
+  ct_1=ct_1+i
+}
+print(ct_1)
+Here loop is used to create sum of all values in range form 
+1 to 10, againg , R woudl allow to perform the
+same result with sum() fucntion.
+Now use for loop and sum and prod fucntion to print cummulative sum
+for range form 1 to 10, same result should be 
+achieved with cumsum() and cumprod() functions
+
+
+for (i in 1:5 ){
+    print(sum(1:i))
+}
+for (i in 1:5 ){
+  print(prod(1:i))
+}
+Here i is used as part of vecvtor generation expression
+1:i
+
+
+### 3
+
+We can define more complex arithmetic progression 
+as proxy for iterator in R.
+vector_1<-rnorm(10,5,5)
+Vector contains random numbers , wa want to extract
+values from each even position,
+total number of even values in sequence from 1 to 10 is 5
+
+for (i in seq(from=2,to=10,by=2) ) {
+  print( vector_1[i] )
+}
+
+Now use seq function to extract all numbers with odd position 
+from sequence from 1 to 20
+vector_1<-rnorm(30,5,5)
+
+for (i in seq(from=1,to=20,by=2) ) {
+  print( vector_1[i] )
+}
+ remember than in R indices starts with 1
+ 
+Again, you can easily done the same with  
+
+vector_1[ seq(from=1,to=20,by=2) ]
+
+Full benefits of loopps are about to reveal themselves.
+
+
+### 4
+Common vectorised R ficntions like sum(),mean(),
+cimsum(),cumprod() can adress operations which are
+common in data analysi tasks and involve mathematical operations
+with clear analytical definition.
+
+Assume non trivial task,
+vector_1<-letters[1:10]
+now I want to replace positions of 
+consequtive pairs:
+  a,b,c,d should become : b,a,d,c
+The task may not behave direct practical application 
+in data anlysis , however
+it demonstated that loop woudl allow more diverse
+type of manipylations compare to base fucntions.
+
+
+for ( i in seq(from=2,to=length(vector_1),by=2 ) ) {
+  print(i)
+}   # here we define second index of each pair
+
+for ( i in seq(from=2,to=length(vector_1),by=2 ) ) {
+      buf=vector_1[i-1]
+      vector_1[i-1]=vector_1[i]
+      vector_1[i]=buf
+} 
+print(vector_1)
+
+Notice we introduce buf variable to keep replaced value stored,
+because second operation rewrirtes it.
+
+Now , given 
+vector_1<-c(0,1,1,rep(0,7))
+Generate Fibonacci sequence.
+YOu need to generate fourtg values 
+which woulde be result of
+sum of 2 previous numbert:
+fouth number= third number+second number
+a(t)=a(t-1)+a(t-2)
+2=1+1
+assigh value 2 to forth index of vector_1
+Repeat this operation 7 times, loop sjould start from 
+4[first missing value]
+
+for (i in 4:10) {
+  vector_1[i]<-vector_1[i-1]+vector_1[i-2]
+}
+print(vector_1)
 
 
 
 
-Don’t use a loop when a vectorized alternative exists
-Don’t grow objects (via c, cbind, etc) during the loop - R has to create a new object and copy across the information just to add a new element or row/column
-Allocate an object to hold the results and fill it in during the loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
