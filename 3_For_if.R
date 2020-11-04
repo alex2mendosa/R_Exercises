b3ab8ea72d9d555c569404b45d214ffc9f151644
alex2mendosa <66001736+alex2mendosa@users.noreply.github.com>
2020-11-04 17:50:48 +0200

    Update 3_For_if.R

--- 3_For_if.R
+++ 3_For_if.R
@@ -2,13 +2,16 @@
 # http://web.csulb.edu/~amonge/classes/cecs174/BigJava-EarlyObjects/chap06-loops.html#:~:text=Loop%20statements%20usually%20have%20four,step%2C%20and%20a%20loop%20body.
 # 1
 Combination of for loop and if statements
-are intristic parts of programming languages which allows to 
+is intristic parta of programming languages which allows to 
 define output based on multiple states of input variables.
 
-Ability of programmer to adjust for+if to problem under consideration
-clearly indicates more than average coding skills, and to find a solution
-to challenge withour calling for in buids 
+Ability of programmer to adjust for+if to problem under
+consideration clearly indicates more than average 
+coding skills, as weel as helps to find a solution
+to challenge withour calling for in buid
 functions to sort, search,manupulate data structures. 
+The latter allows development of algorithmic
+thinking beneficially affecting all aspects of coding.
 
 General template with single condition looks like 
 for ( loop_control_variable in iterator) {
@@ -17,58 +20,62 @@ for ( loop_control_variable in iterator) {
 
 Assume vector 
 vector_1<-sample(1:20,10)
-I want to print "Even" text if value in vector is even.
+I want to print "Even" message if value in vector is even.
 
 for (i in vector_1){
   if (i %% 2==0) {print(paste(i,"is Even")) }
-}  or
+}  
+or
 for (i in 1:length(vector_1)  ){
   if (i %% 2==0) {print(paste(i,"is Even")) }
 } 
 
-Now use the sampe approach to print
+Now use same approach to print
 "Odd" for numners in vector_1 which are odd
 
 
 for (i in vector_1){
-  if (i %% 3==0) {print(paste(i,"is Odd")) }
-}  or
+  if (i %% 2!=0) {print(paste(i,"is Odd")) }
+}  
+or
 for (i in 1:length(vector_1)  ){
-  if (i %% 3==0) {print(paste(i,"is Odd")) }
+  if (i %% 2!=0) {print(paste(i,"is Odd")) }
 } 
 
 
 #2
 To learn best practice in coding, it is usefull
 to know how to incorporate full if+else
-  statement in for loop , to aacount for 
-conditions not expected by Conditional_test
+statement in for loop , to account for 
+conditions not expected by main Conditional_test
 
 Assume variable j takes sequentially 
 values from vector_1, vector contains
 sequence of 1 and 0.
-We want to print TRUE iv value equals 1
+We want to print TRUE if value equals 1
 and 0 otherwise.
 
 First the condition:
   if (i==1) {
     print(TRUE)
   } else {print(FALSE)}
+
 vector_1<-sample(0:1,10,replace=TRUE)
 We want loop_control_variable to take sequentially
-values from vector_1, therefor, above condition should be wrapped with
+values from vector_1, therefore, 
+above condition should be wrapped with
 for statement
 
 for (i in vector_1) {
   if (i==1) {
     print(TRUE)
-  } else {print(FALSE)} }
+  } else {print(FALSE)} 
+  }
 
 Now assume vector:
-  vector_1<-sample(1:20,10)
-Now print message "Even" and element loop variabe in 
+vector_1<-sample(1:20,10)
+Now print message "Even" and loop variabe in 
 each iteration of loop, otherwise , print "Odd"
-
 
 for (i in vector_1) {
   if (i %% 2) {
@@ -80,7 +87,7 @@ for (i in vector_1) {
 # 3
 Assume vector with sample data
 vector_1<-sample(1:30,20)
-I want to print element on vector_1
+I want to print element of vector_1
 if number is between 5 and 15
 and divisible by 3 and count them
 
@@ -92,9 +99,10 @@ for (i in 1:length(vector_1) ) {
 }
 print( paste("Total number of elements:",ct)  )
 
-Here besides print we incorporated variable to
+Here besides print() we incorporated variable to
 count our values of interest.
-Each action is written is separated row.
+We also incorporated multiple
+conditions to test each value.
 
 Now use the same approach to 
 count total numebr of element divisible by 4,
@@ -114,13 +122,13 @@ print( paste("Total number of elements:",ct)  )
 
 # 4  
 Greatest Common Divisor is the largest value which is divisible 
-by 2 or more numbers under consideration.
-Assume numbers 345 and 675, if GCD exist it should 
-be 2 tomes lowet tham minimum value in a range,
+by all numbers under consideration without remainder.
+Assume numbers 345 and 675, if GCD exists, it should 
+be at least 2 times lower tham minimum value between 345 and 675,
 otherwise it wount be divisible with remainder of 0 by
-675
+345
 Solutiuon with loop and if is the following:
-  var_1<-345
+var_1<-345
 var_2<-675
 loop_end<-min(var_1,var_2)/2
 gcd<-0
@@ -132,10 +140,14 @@ print(gcd)
 
 
 Now use similar approach to estimate 
-what numbest frm range from 3 to 20 are considered as
+what number from range from 3 to 20 are considered as
 prime numbers. Prime numbers are divisible only by 1 and
-itselves. Therefore , number of divisors from 2 till 
-the number itself minus 1 whould be 0.
+itselves. Therefore, number of divisors from 2 till 
+the number itself minus 1 should be 0.
+Multiple solutions are possible.
+YOu can apply sum() function to logical vector
+which resukts from test :
+  sum( 5 %% (1:10)==0 )
 
 for (i in 3:20) {
   if ( sum(i %% (2:(i-1))==0)==0) {
@@ -144,25 +156,23 @@ for (i in 3:20) {
 }
 
 Here i %% (2:(i-1))==0 creates  vector of logical type,
-each element is TRUE or FALSE. As 0 is nueric equivalent of
-0, we expect that of value is prime, sum woudl be 0.
-
-
+each element is either TRUE or FALSE. As 0 is numeric equivalent of
+FALSE, we expect that if number is prime, sum would be 0.
 
 
 # 5
 
 Assume string
 str_1<-"sample text which contains several vowels"
-Unlike Python in R we cant loop through 
+Unlike Python, in R we cant loop through 
 string characters or subset particular element like srt_1[2].
 
-First we store each element of srting as selarate element
+First we store each element of srting as separate element
 of character vector:
   
   str_2<-unlist( strsplit(str_1,split="") )
 
-Unlist is requred to conver list which result from strsplit(str_1,split="")
+Unlist is requred to convert list, which result from strsplit(str_1,split=""),
 to vector
 
 sum_vowels<-0
@@ -178,8 +188,8 @@ We counted number of vowels in sentence.
 in vector c("a", "e", "i", "o", "u")
 
 
-Now utf8ToInt() fucntion in R would depict
-symbol coding  in utf8 system, fucntion workd the same way
+Now utf8ToInt() function in R would depict
+symbol coding in utf8 system [ingeger value], function works the same way
 as ord() funtion in python.
 Digits are coded with indices from 48 to 57, 1 to 9
 respectively.
@@ -188,6 +198,8 @@ Now use utf8ToInt() to count number and sum of difits in srt_1 via loop
 and as.numeric() to conver string vertion of digit to number
 str_1<-"sample23 text whi67ch contains 89several vowels"  
 str_2<-unlist( strsplit(str_1,split="") )  
+if is requred to check if element is
+number.
 
 sum_digits<-0
 count_digits<-0
@@ -204,60 +216,69 @@ print( paste(sum_digits,count_digits)  )
 
 
 #6 
-Common exerciser to practice for and if loops is to write the following
+Common exercise to practice for and if loops is to write the following
 code:
   if value in range is multiple of 5, print
-word Buzz, if values is multiple of 3, print Fizz,
+word "Buzz", if value is multiple of 3, print "Fizz",
 if value is multiple of borth numbers , print 
-"FizzBuzz", if statemnts should be collected via
+"FizzBuzz", if statements should be nested via
 else if statements.
-Notice we are alnikg about 3 possible outcome, if 
-condition 
-if (15 %% 3 == 0) {print("Fizz")
-} else if (15 %% 5 == 0) {print("Buzz")
-} else if (15 %% 3 == 0 and 15 %% 5 == 0) {print("FizzBuzz")
+Notice, we are talking about 3 possible outcomes:
+  
+if (15 %% 3 == 0) {
+  print("Fizz")
+} else if (15 %% 5 == 0) {
+  print("Buzz")
+} else if (15 %% 3 == 0 & 15 %% 5 == 0) {
+  print("FizzBuzz")
 } 
 
-15 is multiple of 3 ,5 and both however, as soon as condition 
-if (15 %% 3 == 0) {print("Fizz") avaluates to true,
+15 is multiple of 3,5 and both, however, as soon as condition 
+if (15 %% 3 == 0) {print("Fizz") evaluates to TRUE,
   we would skip the remaining conditions even though 
   they satisfy TRUE outcome as well.
-  We should rearrange else if statements to capturefirst 
-  most general conditions and then procedd to more
-  specific conditions.
+  We should rearrange else if statements to capture first 
+  most general condition.
   Assume vectore 
   
   if (15 %% 3 == 0 & 15 %% 5 == 0) {print("FizzBuzz") 
   } else if (15 %% 5 == 0) {print("Buzz")
   } else if (15 %% 3 == 0 ) {print("Fizz") }
   
-  
-  
+
   Now write loop with for statement and also add 
-  esle statemtn at the end to account to remainig numbers
+  else transition  in the end to account to remainig numbers
+  which are neither divisible by 3 or 5 or both
   
- for (i in sample(20:100,10)  ) { 
-        if (i %% 3 == 0 & i %% 5 == 0) {print("FizzBuzz") 
-  } else if (i %% 5 == 0) {print("Buzz")
-  } else if (i %% 3 == 0 ) {print("Fizz")
-  } 
-   }
+  for (i in sample(20:100,10)  ) { 
+    if (i %% 3 == 0 & i %% 5 == 0) {
+      print("FizzBuzz") 
+    } else if (i %% 5 == 0) {
+      print("Buzz")
+    } else if (i %% 3 == 0 ) {
+      print("Fizz")
+    }  else {
+      print("Exception")
+    }
+  }
   
   
   #7
   Assume vector with random data
   vector_1<-sample(20:100,30)
   
-find sum of all even numbers between 30 and 90
-Combine loop structure and if condition, 
-sum should be collected in variable sum_even
-sum_even<-0
-
-for (i in vector_1) {
-  if (i %% 2==0 & i>30 & i<90) {
-    sum_even<-sum_even+i
+  Find sum of all even numbers between 30 and 90
+  Combine loop structure and if condition, 
+  sum should be collected in variable sum_even
+  sum_even<-0
+  
+  
+  Solution
+  for (i in vector_1) {
+    if (i %% 2==0 & i>30 & i<90) {
+      sum_even<-sum_even+i
+    }
   }
-}
   print(sum_even)
   
   
@@ -269,7 +290,7 @@ for (i in vector_1) {
   from vector_1 in vector_even
   and all odd numbers in vector_odd
   
-  Example solution is
+  Example:
   vector_even<-as.numeric()
   vector_odd<-as.numeric()
   
@@ -277,101 +298,86 @@ for (i in vector_1) {
     if (i %% 2==0) {
       vector_even<-c(vector_even,i) 
     } else { vector_odd<-c(vector_odd,i) }
-    }
-
- This example shows one of the pitfall of
- loops application in R. Operation 
- vector_even<-c(vector_even,i)  creates copy of vector
- vector_even and reassignes previous data in vector_even.
- The operation itself could extremely constly in terms
- of computatons resources requirments.
+  }
+  
+  This example shows one of the pitfalls of
+  loops application in R. Operation 
+  vector_even<-c(vector_even,i)  creates copy of vector
+  vector_even and reassignes previous data in vector_even.
+  The operation itself could be extremely costly in terms
+  of computatons resources requirements.
+  
+  Better solution is to create vectors in advance and 
+  each time new  value appers assign it acocording to 
+  index. However it would require to know in advance 
+  total number of values which will occupy vector_even
+  or vector_odd.
+  
+  # 9
+  Assume te followinf grading scale:
+    
+  Grade	Description
+  E	Excellent
+  V	Very Good
+  G	Good
+  A	Average
+  F	Fail
+  
+  Grades are stored in vector
+  vector_grades<-sample(c("E","V","G","A","F") ,100,replace=TRUE)
+  
+  Now combine loop and if statements to 
+  print  grade and its description.
+  If statements should be independent of each other
+  [dont use else if o else transition]
+  
+  for (i in vector_grades){
+    if (i=="E") {print("Excellent")}
+    if (i=="V") {print("Very Good")}
+    if (i=="G") {print("Good")}
+    if (i=="A") {print("Average")}
+    if (i=="F") {print("Fail")}
+  }
+  
+  Now rewrite example combining conditions with else if
+  statements
+  
+  for (i in vector_grades){
+    if (i=="E") {
+      print("Excellent")
+    } else if (i=="V") {
+      print("Very Good")
+    } else if (i=="G") {
+      print("Good")
+    } else  if (i=="A") {
+      print("Average")
+    } else if (i=="F") {print("Fail") }
+  }
+  
  
- Better solution is to create vectors in advance and 
- each time new  value appers assign it acocording to 
- index. However it would require to know in advance 
- total number of values which will occupy vector_even
- or vector_odd.
- 
-# 9
-Assume te followinf grading scale:
 
-Grade	Description
-E	Excellent
-V	Very Good
-G	Good
-A	Average
-F	Fail
-
-Grades are stored in vector
-vector_grades<-sample(c("E","V","G","A","F") ,100000,replace=TRUE)
-
-Now combine loop and if statements to 
-output grade and its description.
-If statemts should be independent of each other
-[dont use else if o else transition]
-
-for (i in vector_grades){
-  if (i=="E") {print("Excellent")}
-  if (i=="V") {print("Very Good")}
-  if (i=="G") {print("Good")}
-  if (i=="A") {print("Average")}
-  if (i=="F") {print("Fail")}
-}
-
-Now rewrite example combining conditions with else if
-statements
-
-for (i in vector_grades){
-  if (i=="E") {
-    print("Excellent")
-  } else if (i=="V") {
-    print("Very Good")
-  } else if (i=="G") {
-    print("Good")
-  } else  if (i=="A") {
-    print("Average")
-  } else if (i=="F") {print("Fail") }
-}
-
-
-
-#10 !
-Assume vector with random data
-vector_1<-sample(20:100,30)
-
-Loop through each element in vector_1
-and check if number is prime or not.
-Then find sum of all prime numbers in given range
-
-for (i in vector_1) {
-     cand_range<-1:(i-1)
-      if ( sum(i %% cand_range==0)==0 ){
-        print(i)
-      }
-}
-
-
-# 11
- 
-16. Write a Python program to find numbers between
- 100 and 400 (both included) where each digit of a number is an even number.
- The numbers obtained should be printed in a comma-separated sequence
-
-
-
-# 12
- 
- 31. 31. Write a Python program to calculate a dog's age in dog's years. Go to the editor
-Note: For the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years.
-
-
-
-
-# 13
- Write a Python program that reads two integers representing a month and day and prints the season for that month and day.
-
-
-
+  # 10
+  
+  16. Write a Python program to find numbers between
+  100 and 400 (both included) where each digit of a number is an even number.
+  The numbers obtained should be printed in a comma-separated sequence
+  
+  
+  
+  # 11
+  
+  31. 31. Write a Python program to calculate a dog's age in dog's years. Go to the editor
+  Note: For the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years.
+  
+  
+  
+  
+  # 12
+  Write a Python program that reads two integers representing a month and day and prints the season for that month and day.
+  
+  
+  
+  
   
   
   
